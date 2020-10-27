30f7d7c94a82fb23ec27faa49f85caccb71e68ca
Renato Souza Neto <renatosneto0127@protonmail.com>
2020-10-27 14:27:20 -0300

    Feature: Api connection stabilished, add/remove features available

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
+import { v4 as uuidv4 } from 'uuid';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',  {
+      id: uuidv4(),
+      title: `React`,
+      likes: 0
+    })
+    setRepositories([...repositories, response])
+    return repositories
+
   }
 
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [repositories])
+
   async function handleRemoveRepository(id) {
-    // TODO
+    const repId = repositories.filter((repo) => repo.id !== id)
+    await api.delete(`/repositories/${id}`)
+    setRepositories([...repositories])
+    return repositories
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => <li key={repository.id}>{repository.title}<button onClick={() => handleRemoveRepository(repository.id)}>Remover</button></li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
